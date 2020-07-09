6ca3417ad112c079044293984c41dcc37d8d38ae
Eric <ericjamesjameson@gmail.com>
2020-07-09 12:38:47 -0500

    fix spelling issues

--- app/views/interaction.py
+++ app/views/interaction.py
@@ -1,7 +1,7 @@
 from app import app
 
 
-@app.rotue('/slack/interactive-endpoint', methods=['POST'])
+@app.route('/slack/interactive-endpoint', methods=['POST'])
 def slack_interaction():
     print('An Interaction happend')
     return 'An interaction happened'
