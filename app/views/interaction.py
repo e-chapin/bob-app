c9c25836cb6cbb2b3beb59d45d7c70fa60e3972c
Eric <ericjamesjameson@gmail.com>
2020-07-09 14:49:34 -0500

    just print out payload to get from heroku logs

--- app/views/interaction.py
+++ app/views/interaction.py
@@ -13,20 +13,23 @@ from app import slack_client
 def slack_interaction():
     payload = json.loads(request.form["payload"])
 
-    # Check to see what the user's selection was and update the message
-    selection = payload["actions"][0]["selected_options"][0]["value"]
+    from pprint import pprint
+    pprint(payload)
 
-    if selection == "war":
-        message_text = "The only winning move is not to play.\nHow about a nice game of chess?"
-    else:
-        message_text = "response"
-
-    response = client.api_call(
-      "chat.update",
-      channel=payload["channel"]["id"],
-      ts=payload["message_ts"],
-      text=message_text,
-      attachments=[]
-    )
+    # # Check to see what the user's selection was and update the message
+    # selection = payload["actions"][0]["selected_options"][0]["value"]
+    #
+    # if selection == "war":
+    #     message_text = "The only winning move is not to play.\nHow about a nice game of chess?"
+    # else:
+    #     message_text = "response"
+    #
+    # response = client.api_call(
+    #   "chat.update",
+    #   channel=payload["channel"]["id"],
+    #   ts=payload["message_ts"],
+    #   text=message_text,
+    #   attachments=[]
+    # )
 
     return make_response("", 200)
