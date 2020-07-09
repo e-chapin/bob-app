aac077db9ae101765ae702c1e280d5d005cdd412
Eric <ericjamesjameson@gmail.com>
2020-07-09 14:44:11 -0500

    fix matching variables

--- app/views/interaction.py
+++ app/views/interaction.py
@@ -14,7 +14,7 @@ def slack_interaction():
     payload = json.loads(request.form["payload"])
 
     # Check to see what the user's selection was and update the message
-    selection = form_json["actions"][0]["selected_options"][0]["value"]
+    selection = payload["actions"][0]["selected_options"][0]["value"]
 
     if selection == "war":
         message_text = "The only winning move is not to play.\nHow about a nice game of chess?"
